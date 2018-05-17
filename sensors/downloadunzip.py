e1281e856c8f8a5d7146e496e3d081f487b952b8
irom77 <aniairom0>
2018-05-17 12:17:09 -0400

    fixing

--- sensors/downloadunzip.py
+++ sensors/downloadunzip.py
@@ -11,23 +11,27 @@ class DownloadSensor(PollingSensor):
         super(DownloadSensor, self).__init__(sensor_service=sensor_service, config=config, poll_interval=poll_interval)
         self._logger = self.sensor_service.get_logger(name=self.__class__.__name__)
 
-    def setup(self):
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    def setup(self):        
         self._url = self._config['base_url'] 
         self._key = self._config['license_key'] 
+        self._date = self._config['license_key'] 
 
 
     def poll(self):        
         self._logger.debug('DownloadSensor dispatching trigger...')
-        date=datetime.datetime.today().strftime('%Y%m%d')
-        payload = {}
+        if self._date:
+            date = self._date
+        else:    
+            date=datetime.datetime.today().strftime('%Y%m%d')
+        payload = {}        
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         request = get(self._url + '&date=' + date + '&suffix=zip&license_key=' + self._key, verify=False)
         self._logger.debug('response {} '.format(request))
         if request.status_code == 200:         
             zip_file = ZipFile(BytesIO(request.content))
             files = zip_file.namelist()
             zip_file.extractall("./etc")
-            payload['files']=files
+            payload['files']=files      
         payload['date']=date 
         payload['response']=request.status_code  
 
