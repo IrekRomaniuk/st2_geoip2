02e5c2fac28ed1ffadf9b39ec54ad2f94eb2b734
irom77 <aniairom0>
2018-05-18 10:55:11 -0400

    adding more rules

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### GeoIP2 pack
 
-Download MAXMIND GeoIP2 country database and publish
+Download MAXMIND GeoIP2 country database and publish on https://st2/GeoIP2.txt
--- actions/pandaspublish.py
+++ actions/pandaspublish.py
@@ -12,7 +12,7 @@ class pandas_publish(Action):
         
         block_countries=_countries.split(',')
         block_countries = [str(country) for country in block_countries]
-        print(block_countries[0], _ips,_loc) 
+        # print(block_countries[0], _ips,_loc) 
         IPs=pd.read_csv("/opt/stackstorm/packs/geoip2/"+path+'/'+_ips)         
         IPs.drop(['registered_country_geoname_id','represented_country_geoname_id','is_anonymous_proxy','is_satellite_provider'],axis=1,inplace=True)
         # print(IPs.head(3))
@@ -20,7 +20,7 @@ class pandas_publish(Action):
         loc.drop(['locale_code','continent_code','is_in_european_union','continent_name'],axis=1,inplace=True)
         # print(loc.head(3))
         result=IPs.merge(loc,left_on='geoname_id',right_on='geoname_id',how='outer')
-        print(result.head(3))
+        # print(result.head(3))
         # print(IPs.shape,loc.shape,result.shape)
         block_subnets_pd=result[result['country_name'].isin(block_countries)]['network']
         # print(block_subnets_pd.head(3))
