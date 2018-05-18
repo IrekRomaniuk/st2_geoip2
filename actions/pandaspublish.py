188af56d7e7131dd588852fbe9158f8d356a7fb9
irom77 <aniairom0>
2018-05-18 07:52:40 -0400

    updated

--- actions/pandaspublish.py
+++ actions/pandaspublish.py
@@ -2,7 +2,7 @@ from st2actions.runners.pythonrunner import Action
 
 class pandas_publish(Action):
        
-    def run(self, date):
+    def run(self, path):
         #_tag = self.config['date']
         #self.logger.info('date of type: {}'.format(type(date)))        
-        return date
+        return path
