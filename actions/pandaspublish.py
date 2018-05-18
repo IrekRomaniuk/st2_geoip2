c748d7f22445f1c97e772abac0ad8b672a011a5b
irom77 <aniairom0>
2018-05-18 08:15:05 -0400

    fixing

--- actions/pandaspublish.py
+++ actions/pandaspublish.py
@@ -3,6 +3,16 @@ from st2actions.runners.pythonrunner import Action
 class pandas_publish(Action):
        
     def run(self, path):
-        #_tag = self.config['date']
-        #self.logger.info('date of type: {}'.format(type(date)))        
+        # path i.e. GeoIP2-Country-CSV_20180515
+        #self.logger.info('date of type: {}'.format(type(date)))   
+        _countries = self.config['countries']
+        _ips = self.config['ips']
+        _loc = self.config['loc']
+        
+        block_countries=_countries.split(',')
+        IPs=pd.read_csv(_ips) 
+        print(block_countries)
+        print(_ips)
+        print(_loc)
+        print(IPs.head(3))  
         return path
